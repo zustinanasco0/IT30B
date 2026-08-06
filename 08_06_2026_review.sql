6c2b81d321f0a0d072cf848e6edf13cd88344028
zustinanasco0 <zustinanasco0@gmail.com>
2026-08-06 18:57:21 +0800

    Initial commit

---
+++ 08_06_2026_review.sql
@@ -0,0 +1,22 @@
+create table students(
+   student_Id int auto_increment primary key,
+   student_Name varchar(50) not null,
+   student_Course varchar(50) not null,
+   student_Year varchar (5) not null,
+
+
+);
+
+ create table book (
+    book_Id int autoincrement primary key,
+    book_Name varchar(50) not null,
+
+
+ ); 
+
+ create table borrow(
+    borrow_Id int auto_increment primary key
+    student_Id int not null,
+    book_Id int not null,
+
+ );
