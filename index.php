315950dd8609e18275ff8d6a7e229f4ca8cd1691
zustinanasco <zustinanasco0@gmail.com>
2026-09-17 08:05:06 +0800

    Initial db connection for feature/07-initial/-frontend

--- index.php
+++ index.php
@@ -1,69 +1,49 @@
-<?php
-// 1. Connect to Database
-$conn = mysqli_connect("localhost", "root", "", "library_db");
+<?php 
+// Database Connection 
 
-$user_id = "root";
-$user_email = "root";
+$host = 'location';
+$db = 'it30b_lab_db';
+$user = 'root';
+$pass = ' ' ;
+$charset = 'utf8mb4' ;
 
-// 2. Button List
-$buttons = [
-    'Add Book',
-    'Register Student',
-    'Issue Borrow',
-    'View Records'
-];
+$dsn ="msql:host=$host; dbname=$db; charset=$charset";
 
-// 3. Database Action Function
-function logActivity($conn, $user_id, $user_email, $action, $status) {
-    if ($action == 'Add Book') {
-        return mysqli_query($conn, "INSERT INTO books (book_title, book_author, book_category) VALUES ('New Book', 'Author', 'General')");
-    } 
-    if ($action == 'Register Student') {
-        return mysqli_query($conn, "INSERT INTO students (student_first_name, student_last_name, student_course) VALUES ('Alex', 'Smith', 'BSIT')");
-    } 
-    if ($action == 'Issue Borrow') {
-        return mysqli_query($conn, "INSERT INTO borrow (student_id, book_id) VALUES (20240888, 1)");
-    } 
-    if ($action == 'View Records') {
-        return mysqli_query($conn, "SELECT * FROM books");
-    }
-    return false;
+$options = [
+ PDO:: ATTR_ERR => PDO  :: ERRMODE_EXCEPTION,
+ PDO:: ATTR_DEFAULT_FETCH_MODE => PDO ::FETCH_ASSOC,
+ PDO:: ATTR_EMULATE_PREPARES => false,
+
+
+
+]
+
+try{
+
+$pdo = new PDO ($dsn,$user,$pass,$options);
+echo 'connection successful' ;
+}catch(PDOException $e){
+  die("Database connection failed") .$e->getMessage();
+
+
 }
-?>
 
 
-<table border="1" cellpadding="10">
-    <tr>
-        <th>Action</th>
-        <th>Test</th>
-    </tr>
 
-    <?php foreach($buttons as $button) : ?>
-    <tr>
-        <td><?= $button ?></td>
-        <td>
-            <form method="post">
-                <input type="hidden" name="action" value="<?= $button ?>">
-                <button type="submit">Test</button>
-            </form>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
 
-<?php
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $action = $_POST['action'] ?? "test_activity";
-    $status = rand(0, 1) == 1 ? 'success' : 'failed';
-
-    $success = logActivity($conn, $user_id, $user_email, $action, $status);
-
-    if ($success) {
-        echo "<p>Activity: " . $action . " | Status: " . $status . " | Log inserted successfully</p>";
-    } else { 
-        
-        echo "<p>failed to insert activity log</p>";
-    }
-}
-?>
+</body>
+</html>
